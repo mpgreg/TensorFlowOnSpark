cb1fb4442d9d46e6dae140faeb5c4d7512c9d12b
Michael Gregory <mgregory@cloudera.com>
2017-05-26 18:52:43 +0000

    2

--- setup-env.py
+++ setup-env.py
@@ -1,4 +1,4 @@
-!conda create -n tf_env --copy -y -q python=2
-!conda install -n tf_env -y python=2.7.11
+#!conda create -n tf_env --copy -y -q python=2
+!conda install -n tf_env -y python=2.7.12
 !bash ./pip-install-pydoop.sh
 !conda install -n tf_env -y -c conda-forge tensorflow
